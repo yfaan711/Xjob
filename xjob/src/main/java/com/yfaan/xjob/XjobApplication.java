d6e7451dcf016a7cbabae71ffc8fe3b160cfd9d6
YFaan <RYF6409@126.com>
2025-10-26 16:47:00 +0800

    create project

---
+++ xjob/src/main/java/com/yfaan/xjob/XjobApplication.java
@@ -0,0 +1,13 @@
+package com.yfaan.xjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XjobApplication.class, args);
+    }
+
+}
---
+++ xjob/src/test/java/com/yfaan/xjob/XjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yfaan.xjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
